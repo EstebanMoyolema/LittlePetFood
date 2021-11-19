0f0ee5cbde5c895a99bdab11fd0245c597065c16
Esteban <moyolemaesteban99@gmail.com>
2021-11-19 12:48:10 -0500

    Calcular comida

--- sources/js/getData.js
+++ sources/js/getData.js
@@ -1,6 +1,6 @@
 
 function val() {
-    //Perro o gato
+//Perro o gato
 var dat1 = document.getElementsByClassName('A2 active')[0].id;
 //Nombre mascota
 var dat2 = document.getElementById('namePet').value;
@@ -23,5 +23,76 @@ var dat10 = document.getElementsByClassName('A7 active')[0].id;
 //Pasar nombre de la mascota al modal 
 document.getElementById('nombreResult21').value = dat2;
 document.getElementById('nombreResult22').value = dat2;
+
+//Calculadora
+var Kgtog=0;
+//Cachorro
+//--Raza Pequeña--
+if(dat3 == 'razaP'){
+    if(dat5 < 9 &&  dat4 == 0){
+        var Kgtog = dat6*1000;
+        var alimento = (Kgtog*1.3)/100;
+        document.getElementById('grpordia').value = alimento;
+        var megajoul = 0.38*dat6;
+        var kcal = (megajoul*239.1);
+        document.getElementById('kcalpordia').value = kcal.toFixed(0);
+    }
+}
+//--Raza Mediana--
+if(dat3 == 'razaM'){
+    if(dat5 < 6 &&  dat4 == 1){
+        var Kgtog = dat6*1000;
+        var alimento = (Kgtog*1.6)/100;
+        document.getElementById('grpordia').value = alimento;
+        var megajoul = 0.40*dat6;
+        var kcal = (megajoul*239.1);
+        document.getElementById('kcalpordia').value = kcal.toFixed(0);
+    }
+}
+//--Raza Grande--
+if(dat3 == 'razaG'){
+    if(dat4 <= 2){
+        var KgtoG = dat6*1000;
+        var alimento = (KgtoG*1.8)/100;
+        document.getElementById('grpordia').value = alimento;
+        var megajoul = 0.45*dat6;
+        var kcal = (megajoul*239.1);
+        document.getElementById('kcalpordia').value = kcal.toFixed(0);
+    }
+}
+//Adulto
+//--Raza Pequeña--
+if(dat3 == 'razaP'){
+    if(dat5 >= 9 ||  dat4 > 0){
+        var KgtoG = dat6*1000;
+        var alimento = (KgtoG*2)/100;
+        document.getElementById('grpordia').value = alimento;
+        var megajoul = 0.48*dat6;
+        var kcal = (megajoul*239.1);
+        document.getElementById('kcalpordia').value = kcal.toFixed(0);
+    }
+}
+//--Raza Mediana--
+if(dat3 == 'razaM'){
+    if(dat5 >= 6 &&  dat4 >= 1){
+        var KgtoG = dat6*1000;
+        var alimento = (KgtoG*2.3)/100;
+        document.getElementById('grpordia').value = alimento;
+        var megajoul = 0.50*dat6;
+        var kcal = (megajoul*239.1);
+        document.getElementById('kcalpordia').value = kcal.toFixed(0);
+    }
+}
+//--Raza Grande--
+if(dat3 == 'razaG'){
+    if(dat4 > 2){
+        var KgtoG = dat6*1000;
+        var alimento = (KgtoG*2.5)/100;
+        document.getElementById('grpordia').value = alimento;
+        var megajoul = 0.52*dat6;
+        var kcal = (megajoul*239.1);
+        document.getElementById('kcalpordia').value = kcal.toFixed(0);
+    }
+}
     console.log(dat1,dat2,dat3,dat4,dat5,dat6,dat7,dat8,dat9,dat10);
 }
